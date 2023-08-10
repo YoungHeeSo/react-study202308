4e42cb9e804657de077652a358a803a9d98511cb
202216008 <apdlfdldjqtm@gmail.com>
2023-08-10 14:11:29 +0900

    jsx 기초 학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  // const $h2=React.createElement('h2', null, '반가워요!~');
+  const $h2=<h2>방가방가</h2>;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    // <React.Fragment>
+      <>
+      <div className="App">
+        <h1>메롱멜오</h1>
+        {$h2}
+      </div>
+
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 태풍이 와서 비바람이 불었다 <br /> 
+          졸리다
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      </>
+    // </React.Fragment>
   );
 }
 
 export default App;
+
+//닫는 태그가 없다면 < />으로 써
