06f985b19be02ab1b87d6f20ea0c0ab363467435
202216008 <apdlfdldjqtm@gmail.com>
2023-08-11 09:29:52 +0900

    props 기초

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-
 import React from 'react';
-
 import './App.css';
-
 import ExpenseItem from './Components/ExpenseItem';
 
-const App=() =>{
-  
+const App = () => {
+
+  //지출 항목 객체 배열
+  const expense=[
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 7, 7)
+    },
+    {
+      title:"샤인머스켓",
+      price:15000, 
+      date:new Date(2023, 8, 9)
+    },
+    {
+      title: '도미노피자',
+      price: 35000,
+      date: new Date(2023, 7, 14)
+    }
+
+  ];
+
   return (
     <>
-      <ExpenseItem />
+      <ExpenseItem 
+      title={expense[0].title} 
+      price={expense[0].price} 
+      date={expense[0].date} />
+
+      <ExpenseItem 
+      title={expense[1].title} 
+      price={expense[1].price} 
+      date={expense[1].date} />
+
+      <ExpenseItem 
+      title={expense[2].title} 
+      price={expense[2].price} 
+      date={expense[2].date} />
+
     </>
   );
-}
+};
+
+// props 타입, 개수 상관없이 넣을 수 있다
 
 export default App;
-
-
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -3,36 +3,36 @@ import React from 'react';
 //css로딩
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({title, price: propsProps, date}) => {
 
-  const expenseDate=new Date(2023, 8, 4);
-  const expenseTitle = '냠냠치킨';
-  const expensePrice = 19000;
+  // console.log(porps);
+
+  // const expenseDate=date;
+  // const expenseTitle = title;
+  // const expensePrice = propsProps;
 
   //한 자리 숫자를 두 자리 숫자로 변환하는 함수 
   const makeTwodigit=(text)=>{
-    return text.toString().padStart(2, '0');
-    
+    return text.toString().padStart(2, '0');    
   }
 
   //날짜 포멧팅 변환함수 정의
   const makeFormattedDate=()=>{
-    const year = expenseDate.getFullYear(); //년도만 가져와
-    const month = expenseDate.getMonth(); //년도만 가져와
-    const date = expenseDate.getDate(); //년도만 가져와
+    const year = date.getFullYear(); //년도만 가져와
+    const month = date.getMonth(); //년도만 가져와
+    const day = date.getDate(); //년도만 가져와
 
-    return `${year}년${makeTwodigit(month)}월${makeTwodigit(date)}일`
-    //
+    return `${year}년${makeTwodigit(month)}월${makeTwodigit(day)}일`
   }
 
   //숫자를 원화표기법으로 바꾸기
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(expensePrice);
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(propsProps);
 
   return (
     <div className="expense-item">
       <div>{makeFormattedDate()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
       </div>
     </div>
