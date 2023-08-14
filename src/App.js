1bc6d0646813108b182160df6687129c3bc662ca
202216008 <apdlfdldjqtm@gmail.com>
2023-08-14 11:29:00 +0900

    변경 사항 reset

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import Fake_DB_server from './Fake_DB_server';
 
 
 const App = () => {
+   //지출 항목 객체 배열
+   const expenses=[
+    {
+    title: '샤인머스켓',
+    price: 12000,
+    date: new Date(2023, 3 - 1, 23)
+    },
+    {
+    title: 'BBQ치킨',
+    price: 20000,
+    date: new Date(2022, 5 - 1, 21)
+    },
+    {
+    title: '도미노피자',
+    price: 35000,
+    date: new Date(2023, 7 - 1, 14)
+    },
+];
 
   console.log('app실행');
 
@@ -19,7 +37,7 @@ const App = () => {
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Fake_DB_server />
+      <ExpenseList items={expenses} />
     </>
   );
 };
