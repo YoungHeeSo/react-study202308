27dae6acfb0e5857d29ffb1991e6c59c32341ed5
202216008 <apdlfdldjqtm@gmail.com>
2023-08-17 11:16:29 +0900

    목표설정 앱 - 기본 구성 -추가기능 -삭제기능

--- src/Components/CourseGolas/CourseList.js
+++ src/Components/CourseGolas/CourseList.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './CourseList.css'
+import CourseItem from './CourseItem';
 
-const CourseList = () => {
+const CourseList = ({items, onDelete}) => {
   return (
-    <ul className='goal-list'></ul>
-  )
-}
+    <ul className='goal-list'>
+        {items.map((item)=>{
+            return <CourseItem item={item} key={item.id} onDelete={onDelete}/>
+        })}
+    </ul>
+  );
+};
 
 export default CourseList
