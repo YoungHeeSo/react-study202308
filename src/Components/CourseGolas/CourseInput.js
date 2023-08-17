9fe20ced282ccee18bd4c8cc9470b4775884f9a5
202216008 <apdlfdldjqtm@gmail.com>
2023-08-17 11:26:46 +0900

    동적 인라인 스타일 변경

--- src/Components/CourseGolas/CourseInput.js
+++ src/Components/CourseGolas/CourseInput.js
@@ -5,8 +5,15 @@ import Button from '../UI/Button/Button';
 const CourseInput = ({onAdd}) => {
 
     const [enteredText, setEnteredText] =useState('');
+    
+    //입력값 검증 상태변수
+    const [isValid, setIsValid]=useState(true);
 
     const textChangeHandler=e=>{
+        if(e.target.value.trim().length > 0){
+            setIsValid(true);
+        }
+
         // console.log(e.target.value);
         setEnteredText(e.target.value);
     }
@@ -16,6 +23,11 @@ const CourseInput = ({onAdd}) => {
         // console.log(enteredText);
         // console.log('폼 제출');
 
+        if(enteredText.trim().length===0){
+            setIsValid(false);
+            return;
+        }
+
         onAdd(enteredText);
         setEnteredText('');
     }
@@ -24,7 +36,13 @@ const CourseInput = ({onAdd}) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" onChange={textChangeHandler} value={enteredText}/>
+        <input 
+        //동적 인라인 스타일 설정하기
+        style={{
+            background: isValid ? 'transparent':'salmon', 
+            borderColor: isValid ? 'black':'red'
+        }}
+        type="text" onChange={textChangeHandler} value={enteredText}/>
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
